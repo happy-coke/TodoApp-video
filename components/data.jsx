98be72dde6d8aa43988cdfa1cb7439ea46886fe7
happy-coke <www.keeen@gmail.com>
2022-10-05 21:12:45 +0900

    first commit

---
+++ components/data.jsx
@@ -0,0 +1,10 @@
+import { experimentalAddHardhatNetworkMessageTraceHook } from "hardhat/config";
+import React from "react";
+
+const Data = () => {
+  return(
+    <div>Data</div>
+  )
+}
+
+export default Data
